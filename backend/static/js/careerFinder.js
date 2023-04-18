4716d2d79b33a41cf51297f4160f61031d7baaa5
Md Karim <karimmdm1@gmail.com>
2023-04-17 22:13:11 -0400

    showing graphs. spacing issue

--- backend/static/js/careerFinder.js
+++ backend/static/js/careerFinder.js
@@ -14,35 +14,35 @@ function drawSpiderChart(cardEltId, data) {
     console.log(width, height);
 
 
-    // // X axis
-    // var x = d3.scaleBand()
-    //     .range([0, width])
-    //     .domain(data.map(function (d) { return d.Country; }))
-    //     .padding(0.2);
-    // svg.append("g")
-    //     .attr("transform", "translate(0," + height + ")")
-    //     .call(d3.axisBottom(x))
-    //     .selectAll("text")
-    //     .attr("transform", "translate(-10,0)rotate(-45)")
-    //     .style("text-anchor", "end");
+    // X axis
+    var x = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map(function (d) { return d[0]; }))
+        .padding(0.2);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
 
-    // // Add Y axis
-    // var y = d3.scaleLinear()
-    //     .domain([0, 13000])
-    //     .range([height, 0]);
-    // svg.append("g")
-    //     .call(d3.axisLeft(y));
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
-    // // Bars
-    // svg.selectAll("mybar")
-    //     .data(data)
-    //     .enter()
-    //     .append("rect")
-    //     .attr("x", function (d) { return x(d.Country); })
-    //     .attr("y", function (d) { return y(d.Value); })
-    //     .attr("width", x.bandwidth())
-    //     .attr("height", function (d) { return height - y(d.Value); })
-    //     .attr("fill", "#69b3a2")
+    // Bars
+    svg.selectAll("mybar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x(d[0]); })
+        .attr("y", function (d) { return y(d[1]); })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) { return height - y(d[1]); })
+        .attr("fill", "#69b3a2")
 
 }
 
@@ -57,13 +57,16 @@ const CareerCard = (eltId, occupationText) => {
 
 function CreateCareerCardsTiles(numCards, data) {
     for(let i = 0; i < numCards; i++){
-        console.log(id, counter, data[i]);
         let cardEltId = 'card' + i;
         let card = document.createElement("div");
-        card.innerHTML = CareerCard(cardEltId, text);
+        
+        cardData = JSON.parse(data[i]);
+        console.log(cardEltId, i, cardData);
+
+        card.innerHTML = CareerCard(cardEltId, cardData.job);
         cardGrid.appendChild(card);
-        drawSpiderChart(id, data[counter]);
-        counter += 1;
+
+        drawSpiderChart(cardEltId, cardData.top10);
     }
 }
 
