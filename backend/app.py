f54e32604d97118700d8c837cdbff738513c7835
KassandraJ <kc@outersystems.com>
2023-04-26 16:19:59 -0400

    removed False flag

--- backend/app.py
+++ backend/app.py
@@ -54,7 +54,7 @@ def get_data(download=True):
     csv_handler.generate_onet_dictionary(raw_data_dir)
     return csv_handler.data()
 
-jobs = get_data(False)
+jobs = get_data()
 inv_idx = ort.inverted_index(jobs)
 job_idx_map = ort.job_to_idx(jobs)
 p = inv_idx.get("programming")[0]
